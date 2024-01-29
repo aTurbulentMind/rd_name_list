e936b7069dfc6238dda67ef5a81957473a1cc40f
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-01-29 15:59:09 -0800

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,143 +1,64 @@
 <script>
+	let team = [
+		{ number: '#764', nickname: 'Lazer', fullname: 'Lazer' },
+		{ number: '#23', nickname: 'Girlfriend', fullname: 'Nacho Girlfriend' },
+		{ number: '#0', nickname: 'Freeze', fullname: 'Freezer Burn' },
+		{ number: '#122', nickname: 'Whoops', fullname: 'Whoopsie Daisy' },
+		{ number: '#25', nickname: 'Foxy', fullname: 'Foxy Jocie' },
+		{ number: '#303', nickname: 'Blitz', fullname: 'sugar blitz' },
+		{ number: '#62', nickname: 'Squirrely', fullname: 'Squirrely Temple' },
+		{ number: '#8', nickname: 'Maxx', fullname: 'Mad Maxxine' },
+		{ number: '#309', nickname: 'Cypher', fullname: 'Cypher' },
+		{ number: '#20', nickname: 'Slyde', fullname: 'Slydeways' },
+		{ number: '#515', nickname: 'lit', fullname: 'Literally Katie' },
+		{ number: '#6', nickname: 'chaos', fullname: 'Chaos party of six' },
+		{ number: '#68', nickname: 'Gnome', fullname: "Gaurdin' gnome" },
+		{ number: '#210', nickname: 'Giggles', fullname: 'Hits & Giggles' },
+		{ number: '#05', nickname: 'kat', fullname: 'Kategory 5' },
+		{ number: '#5150', nickname: 'Medic', fullname: 'Mad Medic' },
+		{ number: '#101', nickname: 'Shovey', fullname: 'P.S. I shove You' },
+		{ number: '#117', nickname: 'Nixie', fullname: 'Nixie' },
+		{ number: '#08', nickname: 'stare', fullname: 'Shred Astare' },
+		{ number: '#57', nickname: 'Rizzo', fullname: 'Sonic chorizo' },
+		{ number: '#86', nickname: 'Twist', fullname: 'Twisted Spinster' },
+		{ number: '#17', nickname: "Lil'Dizzy", fullname: 'Dysfunctional Dame' },
+		{ number: '#942', nickname: 'Elsa', fullname: '' }
+	];
+
+	let gameRoster = ['#764', '#23', '#0'];
+
+	let matchRoster = team.filter((player) => gameRoster.includes(player.number));
+
+	function addToGameRoster(number) {
+		if (!gameRoster.includes(number)) {
+			gameRoster = [...gameRoster, number];
+			matchRoster = team.filter((player) => gameRoster.includes(player.number));
+		}
+	}
+
+	function selectAll() {
+		gameRoster = team.map((player) => player.number);
+		matchRoster = [...team];
+	}
 </script>
 
 <grid>
-	<namecard>
-		<number> #764</number>
-		<nickname>Lazer</nickname>
-		<fullname>Lazer</fullname>
-	</namecard>
+	<h2>The next match</h2>
+	{#each matchRoster as player}
+		<namecard>
+			<number> {player.number}</number>
+			<nickname>{player.nickname}</nickname>
+			<fullname> {player.fullname}</fullname>
+		</namecard>
+	{/each}
+	<h2>The whole team</h2>
+	<button on:click={selectAll}>Select All</button>
 
-	<namecard>
-		<number> #23</number>
-		<nickname> Girlfriend</nickname>
-		<fullname> Nacho Girlfriend </fullname>
-	</namecard>
-
-	<namecard>
-		<number> #0</number>
-		<nickname>Freeze</nickname>
-		<fullname>Freezer Burn</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #122</number>
-		<nickname>Whoops </nickname>
-		<fullname>Whoopsie Daisy</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #25</number>
-		<nickname>Foxy </nickname>
-		<fullname>Foxy Jocie</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #303</number>
-		<nickname>Blitz </nickname>
-		<fullname>sugar blitz</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #62</number>
-		<nickname>Squirrely</nickname>
-		<fullname>Squirrely Temple </fullname>
-	</namecard>
-
-	<namecard>
-		<number> #8</number>
-		<nickname>Maxx</nickname>
-		<fullname>Mad Maxxine</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #309</number>
-		<nickname>Cypher</nickname>
-		<fullname>Cypher</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #20</number>
-		<nickname>Slyde </nickname>
-		<fullname>Slydeways </fullname>
-	</namecard>
-
-	<namecard>
-		<number>#515</number>
-		<nickname>lit</nickname>
-		<fullname>Literally Katie </fullname>
-	</namecard>
-
-	<namecard>
-		<number> #6</number>
-		<nickname>chaos</nickname>
-		<fullname>Chaos party of six </fullname>
-	</namecard>
-
-	<namecard>
-		<number>#68</number>
-		<nickname>Gnome </nickname>
-		<fullname> Gaurdin' gnome</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #210</number>
-		<nickname>Giggles</nickname>
-		<fullname> Hits & Giggles </fullname>
-	</namecard>
-
-	<namecard>
-		<number> #05</number>
-		<nickname>kat</nickname>
-		<fullname> Kategory 5 </fullname>
-	</namecard>
-
-	<namecard>
-		<number> #5150</number>
-		<nickname>Medic </nickname>
-		<fullname>Mad Medic</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #101</number>
-		<nickname>Shovey</nickname>
-		<fullname>P.S. I shove You </fullname>
-	</namecard>
-
-	<namecard>
-		<number> #117</number>
-		<nickname>Nixie </nickname>
-		<fullname>Nixie </fullname>
-	</namecard>
-
-	<namecard>
-		<number> #08</number>
-		<nickname>stare</nickname>
-		<fullname>Shred Astare </fullname>
-	</namecard>
-
-	<namecard>
-		<number> #57</number>
-		<nickname>Rizzo</nickname>
-		<fullname>Sonic chorizo</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #86 </number>
-		<nickname>Twist</nickname>
-		<fullname>Twisted Spinster</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #17</number>
-		<nickname>Lil'Dizzy</nickname>
-		<fullname>Dysfunctional Dame</fullname>
-	</namecard>
-
-	<namecard>
-		<number> #942 </number>
-		<nickname>Elsa</nickname>
-	</namecard>
+	{#each team as player}
+		<namecard on:click={() => addToGameRoster(player.number)}>
+			<number> {player.number}</number>
+		</namecard>
+	{/each}
 </grid>
 
 <!-- svelte-ignore css-unused-selector -->
@@ -145,6 +66,9 @@
 	@use 'style/baseCamp';
 	:global(*) {
 		box-sizing: border-box;
+	}
+	h2 {
+		font-size: var(--f_xl);
 	}
 
 	grid {
@@ -156,11 +80,16 @@
 		font-size: var(--f_xxl);
 		text-transform: uppercase;
 
+		h2 {
+			grid-column: span 2;
+		}
+
 		namecard {
 			display: grid;
 			text-align: center;
 			border: 2px dotted #000;
 			font-weight: 500;
+			cursor: pointer;
 
 			number,
 			nickname {
